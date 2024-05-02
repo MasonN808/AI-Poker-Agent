ae716d3d9ca99068f71ab762a19e0d0e3937c64e
MasonN808 <mason.naka@gmail.com>
2024-05-02 15:40:29 -0400

    added requirements and gpu support for MLP

--- MCTS_poker/mlp.py
+++ MCTS_poker/mlp.py
@@ -7,6 +7,18 @@ from sklearn.model_selection import train_test_split
 import json
 
 def train():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
     # Load data
     with open('search_tree_40M_sorted.json', 'r') as f:
         data = json.load(f)
