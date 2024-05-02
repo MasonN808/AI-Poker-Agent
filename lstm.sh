6339a2ef02fd231ae44a403d90407cc447beeee6
MasonN808 <mason.nakamura1@marist.edu>
2024-05-02 14:06:10 -0700

    mad bash scripts for training

---
+++ lstm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SLURM settings for the job submission
+#SBATCH --job-name=lstm           # Name of the job
+#SBATCH --cpus-per-task=5         # Number of CPUs per task
+#SBATCH --mem=20G                # Memory allocated
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks=1                # Number of tasks
+#SBATCH --time=1-00:00:00           # Maximum run time of the job (set to 3 days)
+#SBATCH --qos=scavenger           # Quality of Service of the job
+
+# Activate python environment, if you use one (e.g., conda or virtualenv)
+source .venv/bin/activate
+
+BASE_SCRIPT="/nas/ucb/mason/AI-Poker-Agent/MCTS_poker/lstm.py"
+
+srun -N1 -n1 python3 $BASE_SCRIPT
+
+wait
