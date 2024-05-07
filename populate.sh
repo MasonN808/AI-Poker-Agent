2d804e77c8b59a1f07c8c6b2a6f95dad4cee98ff
MasonN808 <mason.nakamura1@marist.edu>
2024-05-06 19:56:38 -0700

    bash script

--- populate.sh
+++ populate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # SLURM settings for the job submission
-#SBATCH --job-name=populate-50M           # Name of the job
-#SBATCH --cpus-per-task=10         # Number of CPUs per task
-#SBATCH --mem=2G                # Memory allocated
+#SBATCH --job-name=1M           # Name of the job
+#SBATCH --cpus-per-task=5         # Number of CPUs per task
+#SBATCH --mem=60G                # Memory allocated
 #SBATCH --nodes=1                 # Number of nodes
 #SBATCH --ntasks=1                # Number of tasks
-#SBATCH --time=2-00:00:00           # Maximum run time of the job (set to 3 days)
+#SBATCH --time=3-00:00:00           # Maximum run time of the job (set to 3 days)
 #SBATCH --qos=scavenger           # Quality of Service of the job
 
 # Activate python environment, if you use one (e.g., conda or virtualenv)
